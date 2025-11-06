cf63d986f141de291dc78b67f2bf0c944367a8d0
zico24820-dev <zico24820@gmail.com>
2025-11-06 18:37:59 +0900

    feat#3 유저 엔티티 추가 작업

--- src/main/java/com/example/projectlxp/user/entity/User.java
+++ src/main/java/com/example/projectlxp/user/entity/User.java
@@ -3,6 +3,7 @@ package com.example.projectlxp.user.entity;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.example.projectlxp.global.base.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -26,40 +27,43 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // Lombok 적용
 @Entity
 @Table(name = "users")
 @SQLDelete(sql = "UPDATE users SET is_deleted = true WHERE id = ?")
 @SQLRestriction("is_deleted = false")
-public class User {
+public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id", nullable = false, updatable = false)
     private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 20)
     private Role role;
 
-    @Column(nullable = false, length = 20)
-    private String name;
+// 추후 추가 예정
+//    @Column(nullable = false, unique = true, length = 20)
+//    private String nickname;
 
-    @Column(nullable = false, unique = true, length = 20)
-    private String nickname;
-
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = true,  length = 255)
     private String email;
 
-    @Column(name = "hashed_password", nullable = false)
+    @Column(name = "hashed_password", nullable = false, length = 255)
     private String hashedPassword;
 
-    @Column(name = "profile_image")
+    @Column(name = "profile_image" , length = 255)
     private String profileImage;
 
     @Column(name = "is_deleted", nullable = false)
     @ColumnDefault("false")
     private boolean isDeleted = false;
 
+    // ---- 매핑
     @OneToMany(mappedBy = "instructor")
     private List<Course> courses = new ArrayList<>();
 
@@ -68,4 +72,12 @@ public class User {
 
     @OneToMany(mappedBy = "user")
     private List<Review> reviews = new ArrayList<>();
+
+    public User(String name, Role role, String email, String hashedPassword, String profileImage) {
+        this.name = name;
+        this.role = role;
+        this.email = email;
+        this.hashedPassword = hashedPassword;
+        this.profileImage = profileImage;
+    }
 }
