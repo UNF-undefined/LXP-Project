b9580a173be4c611ab0cedc1ed22bc02684eb2a0
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2025-11-06 12:14:40 +0900

    Initial commit

---
+++ src/main/java/com/example/projectlxp/LxpProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectlxp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LxpProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LxpProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectlxp/LxpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectlxp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LxpProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
