60a4ff4f4be51e2dae6ba1b6f12f18f41032c62c
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2025-11-08 00:00:50 +0900

    Merge conflict resolved

--- src/main/java/com/example/projectlxp/user/entity/User.java
+++ src/main/java/com/example/projectlxp/user/entity/User.java
@@ -3,6 +3,7 @@ package com.example.projectlxp.user.entity;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.example.projectlxp.global.base.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
