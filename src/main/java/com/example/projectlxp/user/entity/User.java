a6c35afee5ba8e7fe9dcd726af4a2e9c798ada77
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2025-11-08 00:00:50 +0900

    Merge conflict resolved

--- src/main/java/com/example/projectlxp/user/entity/User.java
+++ src/main/java/com/example/projectlxp/user/entity/User.java
@@ -3,7 +3,6 @@ package com.example.projectlxp.user.entity;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.example.projectlxp.global.base.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -17,14 +16,19 @@ import org.hibernate.annotations.SQLRestriction;
 
 import com.example.projectlxp.course.entity.Course;
 import com.example.projectlxp.enrollment.entity.Enrollment;
+import com.example.projectlxp.global.base.BaseEntity;
 import com.example.projectlxp.review.entity.Review;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // Lombok 적용
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "users")
 @SQLDelete(sql = "UPDATE users SET is_deleted = true WHERE id = ?")
@@ -38,17 +42,17 @@ public class User extends BaseEntity {
     @Column(nullable = false, length = 20)
     private Role role;
 
-// 추후 추가 예정
-//    @Column(nullable = false, unique = true, length = 20)
-//    private String nickname;
+    // 추후 추가 예정
+    //    @Column(nullable = false, unique = true, length = 20)
+    //    private String nickname;
 
-    @Column(nullable = false, unique = true,  length = 255)
+    @Column(nullable = false, unique = true, length = 255)
     private String email;
 
     @Column(name = "hashed_password", nullable = false, length = 255)
     private String hashedPassword;
 
-    @Column(name = "profile_image" , length = 255)
+    @Column(name = "profile_image", length = 255)
     private String profileImage;
 
     @Column(name = "is_deleted", nullable = false)
