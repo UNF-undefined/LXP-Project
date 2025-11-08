4710e655eaef720976bb158c970b39cfe2553647
kimsunwook <kckk116560@gmail.com>
2025-11-08 10:45:35 +0900

    feat: 강의 별 리뷰 조회 기능 추가
    # Conflicts:
    #	src/main/java/com/example/projectlxp/course/repository/CourseRepository.java
    #	src/main/java/com/example/projectlxp/review/dto/ReviewResponseDTO.java
    #	src/main/java/com/example/projectlxp/review/service/ReviewServiceImpl.java
    #	src/main/java/com/example/projectlxp/section/repository/SectionRepository.java

--- src/main/java/com/example/projectlxp/course/repository/CourseRepository.java
+++ src/main/java/com/example/projectlxp/course/repository/CourseRepository.java
@@ -3,5 +3,7 @@ package com.example.projectlxp.course.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.projectlxp.course.entity.Course;
+import org.springframework.stereotype.Repository;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {}
