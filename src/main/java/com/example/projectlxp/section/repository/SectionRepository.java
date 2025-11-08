e79665679aade2786a73006e311ba448c95ce511
JSeHoone <beebea545@gmail.com>
2025-11-08 14:12:45 +0900

    feat : findByCourseIdAndOrderNo 메서드 추가
    - Section내 동일한 CourseId와 OrderNo가 있는지 체크하기 위해서 만들었습니다.

--- src/main/java/com/example/projectlxp/section/repository/SectionRepository.java
+++ src/main/java/com/example/projectlxp/section/repository/SectionRepository.java
@@ -1,9 +1,14 @@
 package com.example.projectlxp.section.repository;
 
-import com.example.projectlxp.section.entity.Section;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.projectlxp.section.entity.Section;
+
 @Repository
 public interface SectionRepository extends JpaRepository<Section, Long> {
+
+    Optional<Section> findByCourseIdAndOrderNo(Long courseId, int orderNo);
 }
