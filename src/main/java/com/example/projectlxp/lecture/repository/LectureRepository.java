2aa1c1045f8cd7c844ca94e677981a289d6db8f8
JSeHoone <beebea545@gmail.com>
2025-11-10 14:06:11 +0900

    feat : SectionId를 기준으로 Lecture를 삭제라는 메서드 추가

--- src/main/java/com/example/projectlxp/lecture/repository/LectureRepository.java
+++ src/main/java/com/example/projectlxp/lecture/repository/LectureRepository.java
@@ -1,9 +1,16 @@
 package com.example.projectlxp.lecture.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.projectlxp.lecture.entity.Lecture;
 
 @Repository
-public interface LectureRepository extends JpaRepository<Lecture, Long> {}
+public interface LectureRepository extends JpaRepository<Lecture, Long> {
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("UPDATE Lecture l SET l.isDeleted = true WHERE l.section.id = :sectionId")
+    void deleteBySectionId(Long sectionId);
+}
