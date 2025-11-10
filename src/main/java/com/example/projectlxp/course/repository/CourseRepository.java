8975dab1228afed9ab96ced24148fe52bb0360c5
dnya0 <annay01794@gmail.com>
2025-11-10 14:19:55 +0900

    refactor: JPQL로 변경

--- src/main/java/com/example/projectlxp/course/repository/CourseRepository.java
+++ src/main/java/com/example/projectlxp/course/repository/CourseRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.projectlxp.course.entity.Course;
@@ -11,7 +12,12 @@ import com.example.projectlxp.course.entity.Course;
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
-    @EntityGraph(attributePaths = {"category", "instructor", "parent"})
+    @Query(
+            "SELECT c FROM Course c "
+                    + "JOIN FETCH c.instructor i "
+                    + "JOIN FETCH c.category cat "
+                    + "LEFT JOIN FETCH cat.parent p "
+                    + "WHERE c.id = :courseId AND cat.id = :categoryId AND i.id = :userId")
     Optional<Course> findByIdAndCategoryIdAndInstructorId(
             Long courseId, Long categoryId, Long userId);
 
