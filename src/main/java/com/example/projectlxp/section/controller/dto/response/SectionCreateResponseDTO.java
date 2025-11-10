09000022c2ec7cd08ae37c063ba3880ea5489b30
JSeHoone <beebea545@gmail.com>
2025-11-10 11:19:20 +0900

    refactor : ResponseDTO의 Validation 라이브러리 제거
    - ResponseDTO를 생성할 때는 검증을 따로 해주지 않기에 불필요한 코드를 제거하였습니다.

--- src/main/java/com/example/projectlxp/section/controller/dto/response/SectionCreateResponseDTO.java
+++ src/main/java/com/example/projectlxp/section/controller/dto/response/SectionCreateResponseDTO.java
@@ -1,20 +1,14 @@
 package com.example.projectlxp.section.controller.dto.response;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
 import lombok.Getter;
 
 @Getter
 public class SectionCreateResponseDTO {
 
-    @NotNull(message = "Section의 ID 값은 필수 입니다.")
     private Long sectionId;
 
-    @NotBlank(message = "섹션의 제목은 빈값이 안됩니다.")
     private String title;
 
-    @NotNull(message = "섹션의 순서는 필수 입니다.")
     private int orderNo;
 
     public SectionCreateResponseDTO(Long sectionId, String title, int orderNo) {
