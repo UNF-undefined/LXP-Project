60d757b58ca68182563cf2d496a011f40777a2de
dnya0 <annay01794@gmail.com>
2025-11-10 11:16:56 +0900

    refactor: `BaseEntity`를 상속받도록 변경, `EntityListeners` 추가

--- src/main/java/com/example/projectlxp/category/entity/Category.java
+++ src/main/java/com/example/projectlxp/category/entity/Category.java
@@ -6,9 +6,6 @@ import java.util.List;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
@@ -19,6 +16,7 @@ import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
 import com.example.projectlxp.course.entity.Course;
+import com.example.projectlxp.global.base.BaseEntity;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -28,11 +26,7 @@ import lombok.Getter;
 @Table(name = "categories")
 @SQLDelete(sql = "UPDATE categories SET is_deleted = true WHERE id = ?")
 @SQLRestriction("is_deleted = false")
-public class Category {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Category extends BaseEntity {
 
     @Column(nullable = false, unique = true, length = 20)
     private String name;
