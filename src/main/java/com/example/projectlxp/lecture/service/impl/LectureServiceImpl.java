2aba438f5ecb0010020513b3e3bfeb5a78b8ce6e
JSeHoone <beebea545@gmail.com>
2025-11-11 10:29:45 +0900

    refactor : ContentService로 책임 분리
    - ContentService 의존성 주입하여 file정보를 가공하는 로직을 분리하였습니다.

--- src/main/java/com/example/projectlxp/lecture/service/impl/LectureServiceImpl.java
+++ src/main/java/com/example/projectlxp/lecture/service/impl/LectureServiceImpl.java
@@ -1,11 +1,5 @@
 package com.example.projectlxp.lecture.service.impl;
 
-import java.io.InputStream;
-
-import org.apache.tika.metadata.Metadata;
-import org.apache.tika.parser.AutoDetectParser;
-import org.apache.tika.parser.ParseContext;
-import org.apache.tika.sax.BodyContentHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -16,8 +10,8 @@ import com.example.projectlxp.global.error.CustomBusinessException;
 import com.example.projectlxp.lecture.controller.dto.UploadFileInfoDTO;
 import com.example.projectlxp.lecture.controller.dto.response.LectureCreateResponseDTO;
 import com.example.projectlxp.lecture.entity.Lecture;
-import com.example.projectlxp.lecture.entity.LectureType;
 import com.example.projectlxp.lecture.repository.LectureRepository;
+import com.example.projectlxp.lecture.service.ContentService;
 import com.example.projectlxp.lecture.service.LectureService;
 import com.example.projectlxp.section.entity.Section;
 import com.example.projectlxp.section.repository.SectionRepository;
@@ -28,11 +22,17 @@ public class LectureServiceImpl implements LectureService {
     private LectureRepository lectureRepository;
     private SectionRepository sectionRepository;
 
+    // LectureService가 ContentService보다 더 상위 개념이어서 같은 Service layer여도 의존성이 가능하다 !
+    private ContentService contentService;
+
     @Autowired
     public LectureServiceImpl(
-            LectureRepository lectureRepository, SectionRepository sectionRepository) {
+            LectureRepository lectureRepository,
+            SectionRepository sectionRepository,
+            ContentService contentService) {
         this.lectureRepository = lectureRepository;
         this.sectionRepository = sectionRepository;
+        this.contentService = contentService;
     }
 
     @Override
@@ -57,7 +57,7 @@ public class LectureServiceImpl implements LectureService {
         }
 
         // convert File Info DTO
-        UploadFileInfoDTO fileInfo = getFileInfo(file);
+        UploadFileInfoDTO fileInfo = contentService.uploadFile(file);
 
         // create Lecture
         Lecture newLecture =
@@ -77,47 +77,5 @@ public class LectureServiceImpl implements LectureService {
                 savedLecture.getTitle(),
                 savedLecture.getType(),
                 savedLecture.getOrderNo());
-    }
-
-    private UploadFileInfoDTO getFileInfo(MultipartFile file) throws Exception {
-        String fileName = file.getOriginalFilename();
-        String fileExt = fileName.substring(fileName.lastIndexOf(".") + 1);
-        String duration = null;
-
-        LectureType type;
-        if (fileExt.equals("mp4")) {
-            type = LectureType.VIDEO;
-        } else if (fileExt.equals("pdf")) {
-            type = LectureType.DOCUMENT;
-        } else {
-            throw new CustomBusinessException("업로드 파일의 형식은 pdf 혹은 mp4만 가능합니다.");
-        }
-
-        if (type.equals(LectureType.VIDEO)) {
-            duration = getVideoDuration(file);
-        }
-
-        // TODO : 임의의 파일 스토리지 저장소
-        String fileURL = "s3:/test/" + fileName;
-        return new UploadFileInfoDTO(fileURL, type, duration);
-    }
-
-    private String getVideoDuration(MultipartFile file) throws Exception {
-        AutoDetectParser parser = new AutoDetectParser();
-        Metadata metadata = new Metadata();
-        ParseContext context = new ParseContext();
-
-        try (InputStream inputStream = file.getInputStream()) {
-            parser.parse(inputStream, new BodyContentHandler(), metadata, context);
-        }
-
-        // TODO : 비디오마다 가지고 있는 메타 정보가 달라서 길이를 추출하지 못함.
-        // 그렇다고 ffempg 라이브러리를 사용하자고 하니 라이브러라 하나가 200Mb된다고 함.(오버엔지니어링)
-        String duration = metadata.get("xmpDM:duration"); // 초 단위
-        if (duration != null) {
-            return duration;
-        } else {
-            throw new CustomBusinessException("비디오 길이를 가져올 수 없습니다.");
-        }
     }
 }
