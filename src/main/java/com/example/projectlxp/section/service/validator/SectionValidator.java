8d13f46f616f0c498414c2b9c5bc3ec93273622c
JSeHoone <beebea545@gmail.com>
2025-11-11 11:19:05 +0900

    refactor : 불필요한 의존성 삭제

--- src/main/java/com/example/projectlxp/section/service/validator/SectionValidator.java
+++ src/main/java/com/example/projectlxp/section/service/validator/SectionValidator.java
@@ -1,31 +1,13 @@
 package com.example.projectlxp.section.service.validator;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-import com.example.projectlxp.course.repository.CourseRepository;
 import com.example.projectlxp.global.error.CustomBusinessException;
-import com.example.projectlxp.lecture.repository.LectureRepository;
 import com.example.projectlxp.section.entity.Section;
-import com.example.projectlxp.section.repository.SectionRepository;
 
 @Component
 public class SectionValidator {
-
-    private final SectionRepository sectionRepository;
-    private final CourseRepository courseRepository;
-    private final LectureRepository lectureRepository;
-
-    @Autowired
-    public SectionValidator(
-            SectionRepository sectionRepository,
-            CourseRepository courseRepository,
-            LectureRepository lectureRepository) {
-        this.sectionRepository = sectionRepository;
-        this.courseRepository = courseRepository;
-        this.lectureRepository = lectureRepository;
-    }
 
     public void validateSectionAuthority(Section section, Long userId) {
         if (section.getCourse().getInstructor().getId() != userId) {
