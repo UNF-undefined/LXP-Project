7f63f90e06ccc75253b6af36b3a6f0ac7160b667
dnya0 <annay01794@gmail.com>
2025-11-12 16:27:23 +0900

    feat: 강좌 관리 API에 INSTRUCTOR 권한 부여 로직 추가

--- src/main/java/com/example/projectlxp/course/controller/CourseController.java
+++ src/main/java/com/example/projectlxp/course/controller/CourseController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -53,12 +54,14 @@ public class CourseController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('INSTRUCTOR')")
     public BaseResponse<CourseResponse> registerCourse(
             @RequestBody @Valid CourseSaveRequest request, @CurrentUserId Long userId) {
         return BaseResponse.success(courseService.saveCourse(request, userId));
     }
 
     @PutMapping("/{courseId}")
+    @PreAuthorize("hasRole('INSTRUCTOR')")
     public BaseResponse<CourseDTO> updateCourse(
             @PathVariable Long courseId,
             @RequestBody CourseUpdateRequest request,
@@ -67,6 +70,7 @@ public class CourseController {
     }
 
     @DeleteMapping("/{courseId}")
+    @PreAuthorize("hasRole('INSTRUCTOR')")
     public BaseResponse<Boolean> deleteCourse(
             @PathVariable Long courseId, @CurrentUserId Long userId) {
         return BaseResponse.success("강좌 삭제 성공", courseService.deleteCourse(courseId, userId));
--- src/main/java/com/example/projectlxp/course/service/validator/CourseValidator.java
+++ src/main/java/com/example/projectlxp/course/service/validator/CourseValidator.java
@@ -7,37 +7,24 @@ import org.springframework.stereotype.Component;
 import com.example.projectlxp.category.error.CategoryNotFoundException;
 import com.example.projectlxp.category.repository.CategoryRepository;
 import com.example.projectlxp.course.entity.Course;
-import com.example.projectlxp.course.error.CourseCreationDeniedException;
 import com.example.projectlxp.course.error.CourseUpdateDeniedException;
-import com.example.projectlxp.course.repository.CourseRepository;
 import com.example.projectlxp.global.error.CustomBusinessException;
-import com.example.projectlxp.user.entity.Role;
-import com.example.projectlxp.user.entity.User;
 import com.example.projectlxp.user.repository.UserRepository;
 
 @Component
 public class CourseValidator {
 
-    private final CourseRepository courseRepository;
     private final UserRepository userRepository;
     private final CategoryRepository categoryRepository;
 
-    public CourseValidator(
-            CourseRepository courseRepository,
-            UserRepository userRepository,
-            CategoryRepository categoryRepository) {
-        this.courseRepository = courseRepository;
+    public CourseValidator(UserRepository userRepository, CategoryRepository categoryRepository) {
         this.userRepository = userRepository;
         this.categoryRepository = categoryRepository;
     }
 
     public void validateCourseCreation(Long categoryId, Long userId) {
-        User user =
-                userRepository
-                        .findById(userId)
-                        .orElseThrow(() -> new CustomBusinessException("존재하지 않는 강사(User) ID입니다."));
-        if (user.getRole() != Role.INSTRUCTOR) {
-            throw new CourseCreationDeniedException();
+        if (!userRepository.existsById(userId)) {
+            throw new CustomBusinessException("존재하지 않는 강사(User) ID입니다.");
         }
 
         if (!categoryRepository.existsById(categoryId)) {
