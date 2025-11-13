e59fc1c0525b50f57464ecc812cf16a0160b94d3
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2025-11-13 10:58:05 +0900

    refactor: spotless 적용

--- src/main/java/com/example/projectlxp/lecture/repository/LectureRepository.java
+++ src/main/java/com/example/projectlxp/lecture/repository/LectureRepository.java
@@ -1,8 +1,8 @@
 package com.example.projectlxp.lecture.repository;
 
 import java.util.List;
-import java.util.Set;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
