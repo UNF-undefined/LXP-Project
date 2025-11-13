6ef69b1968981f2fc1e495966a404489e4b93981
kimsunwook <kckk116560@gmail.com>
2025-11-13 09:44:49 +0900

    refactor: reviewController 임시 id를 @AuthenticationPrincipal User user로 변경

--- src/main/java/com/example/projectlxp/review/controller/ReviewController.java
+++ src/main/java/com/example/projectlxp/review/controller/ReviewController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import jakarta.validation.Valid;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.projectlxp.global.dto.BaseResponse;
@@ -20,6 +20,7 @@ import com.example.projectlxp.global.dto.PageResponse;
 import com.example.projectlxp.review.dto.ReviewRequestDTO;
 import com.example.projectlxp.review.dto.ReviewResponseDTO;
 import com.example.projectlxp.review.service.ReviewService;
+import com.example.projectlxp.user.entity.User;
 
 import lombok.RequiredArgsConstructor;
 
@@ -42,13 +43,10 @@ public class ReviewController {
     public BaseResponse<ReviewResponseDTO> createReview(
             @PathVariable Long courseId,
             @Valid @RequestBody ReviewRequestDTO requestDTO,
-
-            // Security 대신 '1번 유저'가 썼다고 가정하고 임시 ID를 받음
-            // (나중에 Security 적용되면 이 파라미터는 @AuthenticationPrincipal User user로 변경됨)
-            @RequestParam(defaultValue = "1") Long tempUserId) {
+            @AuthenticationPrincipal User user) {
 
         ReviewResponseDTO responseDTO =
-                reviewService.createReview(courseId, requestDTO, tempUserId);
+                reviewService.createReview(courseId, requestDTO, user.getId());
 
         return BaseResponse.success(responseDTO);
     }
@@ -56,10 +54,8 @@ public class ReviewController {
     // 리뷰 삭제
     @DeleteMapping("/{reviewId}")
     public BaseResponse<Void> deleteReview(
-            @PathVariable Long reviewId,
-            @RequestParam(defaultValue = "1") Long tempUserId // (임시) 임시 유저 ID를 받음
-            ) {
-        reviewService.deleteReview(reviewId, tempUserId);
+            @PathVariable Long reviewId, @AuthenticationPrincipal User user) {
+        reviewService.deleteReview(reviewId, user.getId());
         return BaseResponse.success(null);
     }
 
@@ -68,11 +64,10 @@ public class ReviewController {
     public BaseResponse<ReviewResponseDTO> updateReview(
             @PathVariable Long reviewId,
             @Valid @RequestBody ReviewRequestDTO requestDTO,
-            // (임시) '본인 확인'을 위한 유저 ID
-            @RequestParam(defaultValue = "1") Long tempUserId) {
+            @AuthenticationPrincipal User user) {
 
         ReviewResponseDTO responseDTO =
-                reviewService.updateReview(reviewId, requestDTO, tempUserId);
+                reviewService.updateReview(reviewId, requestDTO, user.getId());
 
         return BaseResponse.success(responseDTO);
     }
